7f3bd9c67c4bbb8a317b158f7750250f41d9a693
Siddhantborkar2 <siddhantborkar2@gmail.com>
2025-03-30 20:36:17 +0530

    Delete SQL directory

--- SQL/Schema.sql
+++
@@ -1,5 +0,0 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Link to schema: https://app.quickdatabasediagrams.com/#/d/s8Wnqm
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-
---Card Holder
